bf7a43068f4f238ac703472830555504d28c28b1
txl77 <111854019+txl77@users.noreply.github.com>
2023-09-28 10:42:28 +0200

    Update InstaData.php

--- InstaData.php
+++ InstaData.php
@@ -1,32 +1,29 @@
 <?php
-// Create a new DOMDocument
-$dom = new DOMDocument();
+// Function to extract the title attribute from the HTML
+function extractTitle($html) {
+    // Define a regular expression pattern to match the title attribute
+    $pattern = '/<span class="_ac2a" title="([^"]+)"/';
 
-// Load the HTML content from the URL
-$url = "https://www.instagram.com/uefrance/";
-$html = file_get_contents($url);
+    // Use preg_match_all to find all matches
+    preg_match_all($pattern, $html, $matches);
 
-// Load the HTML content into the DOMDocument
-@$dom->loadHTML($html);
-
-// Use XPath to find the element with the class "_ac2a"
-$xpath = new DOMXPath($dom);
-$elements = $xpath->query('//span[@class="_ac2a"]');
-
-// Initialize an array to store the results
-$results = [];
-
-// Loop through the found elements
-foreach ($elements as $element) {
-    // Get the "title" attribute
-    $title = $element->getAttribute('title');
-    
-    // Add the title to the results array
-    $results[] = $title;
+    // Check if matches were found
+    if (!empty($matches[1])) {
+        // Encode the matches as JSON
+        return json_encode($matches[1]);
+    } else {
+        return json_encode([]);
+    }
 }
 
-// Encode the results array as JSON
-$jsonResult = json_encode($results);
+// URL of the Instagram page
+$url = "https://www.instagram.com/uefrance/";
+
+// Fetch the HTML content from the URL
+$html = file_get_contents($url);
+
+// Extract the title attribute data and return it as JSON
+$jsonResult = extractTitle($html);
 
 // Output the JSON object
 echo $jsonResult;
